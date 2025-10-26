722b3b5bf03f9d48ff21f7d2913f1382a6eaba4b
verytrashpanda <verytrashpanda@gmail.com>
2025-10-26 02:33:38 +0000

    added greyed out console messages

--- utils/pull_request.py
+++ utils/pull_request.py
@@ -1,9 +1,14 @@
 import utils.bot_constants as consts
 import requests
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+
 
 #The function to make a pull request
 def PullRequest(endpoint: str, params: dict) -> dict:
     url = consts.URL_BASE + endpoint
     r = requests.get(url, headers=consts.HEADERS, params=params)
-    print(f"Making GET request to {url}.")
+    print(Fore.LIGHTBLACK_EX + "--> Making GET request to {url}. <--")
     return (r.json())
