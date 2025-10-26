fc2df45ad04f1b231acf8289d3cbcfbc03e89b2f
verytrashpanda <verytrashpanda@gmail.com>
2025-10-26 11:07:57 +0000

    idek what i just did tbh

--- cogs/updates_cog.py
+++ cogs/updates_cog.py
@@ -27,17 +27,17 @@ class UpdatesCog(commands.Cog):
 
     playedCodes = ["1H", "HT", "2H", "ET", "BT", "P"]
 
-    nextWeekFixtures = [] #List of the fixtures from today to next week. Only actively updated when the league is ACTIVE.
+    todayFixtures = [] #List of the fixtures from today to next week. Only actively updated when the league is ACTIVE.
     liveFixtures = []
 
     #This function gets every single game being played from today until next week.
-    async def GetNextWeek(self) -> dict:
+    async def GetTodayFixtures(self) -> dict:
         today = datetime.now() #Get current date and time
         formattedToday = today.strftime("%Y-%m-%d") # Format date as yyyy-mm-dd
         nextWeek = today + timedelta(weeks=1)
         formattedNextWeek = nextWeek.strftime("%Y-%m-%d")
         
-        digest = PullRequest("fixtures", {"league" : 39, "season" : 2025, "from" : formattedToday, "to" : formattedNextWeek})
+        digest = PullRequest("fixtures", {"league" : 39, "season" : 2025, "date" : formattedToday})
         return digest["response"]
     
     async def cog_load(self): #On cog load
@@ -49,34 +49,41 @@ class UpdatesCog(commands.Cog):
     @tasks.loop(minutes=1)
     async def WakeManager(self):
         if self.leagueActive == False:
-            dateISO = self.nextWeekFixtures[0]["fixture"]["date"]
-            nextMatchTime = datetime.fromisoformat(dateISO)
-            timeUntilNextMatch = nextMatchTime - datetime.now(timezone.utc)
-            
-            #If the next match is 1 minute or less away (or has already started)...
-            if timeUntilNextMatch.total_seconds() <= 65:
-                print(Fore.BLUE + "!!! League is ACTIVE !!! \n")
-                self.leagueActive = True
-        if self.leagueActive == True:
-            self.nextWeekFixtures = self.GetNextWeek()
+            try:
+                dateISO = self.todayFixtures[0]["fixture"]["date"]
+                nextMatchTime = datetime.fromisoformat(dateISO)
+                timeUntilNextMatch = nextMatchTime - datetime.now(timezone.utc)
 
-            self.liveFixtures = []
-            #Every minute, refresh liveFixtures
-            for entry in self.nextWeekFixtures:
-                if entry["fixture"]["status"]["short"] in self.playedCodes:
-                    self.liveFixtures.append(entry)
-            
+                #If the next match is 1 minute or less away (or has already started)...
+                if timeUntilNextMatch.total_seconds() <= 65:
+                    print(Fore.BLUE + "!!! League is ACTIVE !!! \n")
+                    self.leagueActive = True
+            except:
+                print("No fixtures today.")
+
+        if self.leagueActive == True:
             #If no matches are live anymore, go to sleep
             if len(self.liveFixtures) == 0:
                 print(Fore.BLUE + "!!! League is INACTIVE !!! \n")
                 self.leagueActive = False
 
-#Match event checker. Checks all events for live matches.
+#This guy's job is to keep the Fixture classes in the list updated.
+@tasks.loop(minutes=1)
+async def FixtureHandler(self):
+    if self.leagueActive == False:
+        return
+    
+    #Get our fixture list from the site.
+    requestedList = self.GetTodayFixtures()
 
-#Always runs in the background. Only iterates if the league is inactive. Keeps nextWeekFixtures updated long-term.
-@tasks.loop(hours=6)
+    #For each entry in the fixture list 
+
+
+
+#Always runs in the background. Only goes forth if the league is inactive. Keeps nextWeekFixtures updated long-term.
+@tasks.loop(hours=2)
 async def SlowChecker(self):
     if self.leagueActive == True:
         return
 
-    self.nextWeekFixtures = self.GetNextWeek()
+    self.todayFixtures = self.GetTodayFixtures()
